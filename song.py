dd150e68c33f7d73afacd25018b05891d036db83
Faiz <86136319+faiz0416@users.noreply.github.com>
2022-10-11 00:43:36 +0800

    Update song.py

--- song.py
+++ song.py
@@ -8,29 +8,39 @@ from random import choice
 import os
 
 def getsong():
+    
+    for i in range(2):
+        if i == 0:
+            url1 = 'https://open.spotify.com/'
+            classname1 = 'tsv7E_RBBw6v0XTQlcRo'
+            classname2 = 'Nqa6Cw3RkDMV8QnYreTr'
+        else:
+            url1 = onesong
+            classname1 = 'HcMOFLaukKJdK5LfdHh0'
+            classname2 = 't_yrXoUO3qGsJS4Y6iXX'
+        
+        chrome_options = webdriver.ChromeOptions()
+        chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+        chrome_options.add_argument("--headless") #無頭模式
+        chrome_options.add_argument("--disable-dev-shm-usage")
+        chrome_options.add_argument("--no-sandbox")
+        driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
 
-    chrome_options = webdriver.ChromeOptions()
-    chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-    chrome_options.add_argument("--headless") #無頭模式
-    chrome_options.add_argument("--disable-dev-shm-usage")
-    chrome_options.add_argument("--no-sandbox")
-    driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
-    driver.get('https://open.spotify.com/playlist/37i9dQZF1DXcBWIGoYBM5M')
+        driver.get(url1)
 
-    WebDriverWait(driver, 30).until(
-        EC.presence_of_element_located(
-            (By.CLASS_NAME, 'HcMOFLaukKJdK5LfdHh0'))
-    )
+        WebDriverWait(driver, 30).until(
+            EC.presence_of_element_located((By.CLASS_NAME, classname1)))
 
-    songs = driver.find_elements(By.CLASS_NAME, 't_yrXoUO3qGsJS4Y6iXX')
-    list_song = []
-    # video-title   wc-endpoint
+        songs = driver.find_elements(By.CLASS_NAME, classname2)
 
-    for song in songs:
-        if song.get_attribute('href') != None:
-            list_song.append(str(song.get_attribute('href')))
-            
-    onesong = choice(list_song)
+        list_song = []
 
-    driver.quit()
-    return onesong
+        for song in songs:
+            if song.get_attribute('href') != None:
+                list_song.append(str(song.get_attribute('href')))
+
+        onesong = choice(list_song)
+
+        return onesong
+        driver.quit()
+    
